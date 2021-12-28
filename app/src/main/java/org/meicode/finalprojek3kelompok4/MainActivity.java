e2dd0f38315a171508c9e887ed29c131f4ca7765
yahyawildan <yahyawildan.id@gmail.com>
2021-12-28 11:06:22 +0700

    mengubah onClick di button dengan menggunakan binding, custom warna button

--- app/src/main/java/org/meicode/finalprojek3kelompok4/MainActivity.java
+++ app/src/main/java/org/meicode/finalprojek3kelompok4/MainActivity.java
@@ -1,6 +1,7 @@
 package org.meicode.finalprojek3kelompok4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import org.mariuszgromada.math.mxparser.*;
 
 import android.os.Bundle;
@@ -27,113 +28,159 @@ public class MainActivity extends AppCompatActivity {
         binding.tvInputAngka.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(getString(R.string.InputAngka).equals(display.getText().toString())){
+                if (getString(R.string.InputAngka).equals(display.getText().toString())) {
                     display.setText("");
                 }
             }
         });
 
-}
+        binding.button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("0");
+            }
+        });
+        binding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("1");
+            }
+        });
 
-    private void updateText(String strToAdd){
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("2");
+            }
+        });
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("3");
+            }
+        });
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("4");
+            }
+        });
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("5");
+            }
+        });
+        binding.button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("6");
+            }
+        });
+        binding.button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("7");
+            }
+        });
+        binding.button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("8");
+            }
+        });
+        binding.button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("9");
+            }
+        });
+        binding.button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("0");
+            }
+        });
+        binding.buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText("");
+            }
+        });
+
+        binding.buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("+");
+            }
+        });
+        binding.buttonMin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("-");
+            }
+        });
+        binding.buttonMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("X");
+            }
+        });
+
+        binding.buttonDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText("÷");
+            }
+        });
+
+        binding.buttonDecimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText(".");
+            }
+        });
+
+        binding.btnAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userExp = display.getText().toString();
+
+                userExp = userExp.replaceAll("÷", "/");
+                userExp = userExp.replaceAll("X", "*");
+
+                Expression exp = new Expression(userExp);
+
+                String result = String.valueOf(exp.calculate());
+                display.setText(result);
+                display.setSelection(result.length());
+            }
+        });
+
+    }
+
+    private void updateText(String strToAdd) {
         String oldStr = display.getText().toString();
         int cursorPos = display.getSelectionStart();
         String leftStr = oldStr.substring(0, cursorPos);
         String rightStr = oldStr.substring(cursorPos);
-        if(getString(R.string.InputAngka).equals(display.getText().toString())){
+        if (getString(R.string.InputAngka).equals(display.getText().toString())) {
             display.setText(strToAdd);
-        }
-        else {
-            display.setText(String.format("%s%s%s", leftStr,strToAdd, rightStr));
+        } else {
+            display.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
         }
         display.setSelection(cursorPos + 1);
 
     }
 
-    public void zeroBtn(View view){
-        updateText("0");
-    }
 
-    public void oneBtn(View view){
-        updateText("1");
-    }
-
-    public void twoBtn(View view){
-        updateText("2");
-    }
-
-    public void threeBtn(View view){
-        updateText("3");
-    }
-
-    public void fourBtn(View view){
-        updateText("4");
-    }
-
-    public void fiveBtn(View view){
-        updateText("5");
-    }
-
-    public void sixBtn(View view){
-        updateText("6");
-    }
-
-    public void sevenBtn(View view){
-        updateText("7");
-    }
-
-    public void eightBtn(View view){
-        updateText("8");
-    }
-
-    public void nineBtn(View view){
-        updateText("9");
-    }
-
-    public void clearBtn(View view){
-        display.setText("");
-    }
-
-    public void penjumlahanBtn(View view){
-        updateText("+");
-    }
-
-    public void penguranganBtn(View view){
-        updateText("-");
-    }
-
-    public void perkalianBtn(View view){
-        updateText("X");
-    }
-
-    public void pembagianBtn(View view){
-        updateText("/");
-    }
-
-    public void decimalBtn(View view){
-        updateText(".");
-    }
-
-    public void answerBtn(View view){
-        String userExp = display.getText().toString();
-
-        userExp = userExp.replaceAll("÷", "/");
-        userExp = userExp.replaceAll("X", "*");
-
-        Expression exp = new Expression(userExp);
-
-        String result = String.valueOf(exp.calculate());
-        display.setText(result);
-        display.setSelection(result.length());
-    }
-
-    public void deleteBtn(View view){
+    public void deleteBtn(View view) {
         int cursorPos = display.getSelectionStart();
         int textlen = display.getText().length();
 
-        if (cursorPos !=0 && textlen !=0){
+        if (cursorPos != 0 && textlen != 0) {
             SpannableStringBuilder selection = (SpannableStringBuilder) display.getText();
-            selection.replace(cursorPos - 1,cursorPos,"");
+            selection.replace(cursorPos - 1, cursorPos, "");
             display.setText(selection);
             display.setSelection(cursorPos - 1);
         }
